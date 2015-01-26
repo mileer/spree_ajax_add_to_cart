94b62c2c6e06792023f0106598eb72ce3304ef6c
Victor Archila <varchila92@gmail.com>
2015-01-26 09:49:42 -0430

    Agrega la validacion de que el usuario debe haber iniciado sesión para poder agregar articulos al carrito de compra.

--- app/overrides/ajax_cart_form_index.rb
+++ app/overrides/ajax_cart_form_index.rb
@@ -1,6 +1,9 @@
 Deface::Override.new(:virtual_path => "spree/shared/_products",
                      :name => 'remote_cart_form',
                      :replace => ".price",
-                     :erb => '<%= button_to Spree.t("add_to_cart"),
-                     		 {:controller => "orders", :action => "populate", :variant_id => product.id, :quantity => 1},
-                     		 {:remote => true, :method=>:post} %>')
+                     :erb => '<% if spree_current_user %>
+                                 <%= button_to Spree.t("add_to_cart"),
+                                 {:controller => "orders", :action => "populate", 
+                                  :variant_id => product.id, :quantity => 1},
+                                 {:remote => true, :method=>:post} %>
+                             <% end %>')
